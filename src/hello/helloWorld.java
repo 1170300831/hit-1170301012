9637a2b1f67c92ca412fb8a258f4edddd1102df5
Honglin Li <L18145192387@163.com>
2019-07-07 18:47:11 +0800

    change some codes

--- src/hello/helloWorld.java
+++ src/hello/helloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello HIT ，you are so clever Wangsq!");
 	}
 
 }
